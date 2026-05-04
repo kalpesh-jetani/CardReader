2a858a2f3d238da66d0a1dcf911aa24f1f647b3b
kalpeshjetani1 <kalpesh.jetani@smartsensesolutions.com>
2026-05-04 11:37:44 +0530

    Initial Commit

---
+++ CardReader/CardReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardReaderApp.swift
+//  CardReader
+//
+//  Created by Kalpesh on 04/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CardReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
